9e729750ebdf39c110fa631d8041df5317ef9627
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-03-13 20:00:17 -0700

    getting the data from the api

--- weather/src/components/MainApp.js
+++ weather/src/components/MainApp.js
@@ -4,7 +4,6 @@ import Form from "./Form";
 const MainApp = () => {
   return (
     <div>
-      <h2>MainApp</h2>
       <div className="container">
         <div className="form-container">
           <Form />
@@ -17,42 +16,42 @@ const MainApp = () => {
             </div>
             <div className="bottom-left">
               <div className="date-info">
-                <span>4: 00 pm</span>
-                <span>fri mar 13 2020</span>
+                <span className="time">4: 00 pm</span>
+                <span className="date">fri mar 13 2020</span>
               </div>
               <div className="main-temp">
-                <span>50.3</span>
+                <span>50.3 c</span>
               </div>
             </div>
           </div>
 
           <div className="top-right">
             <div className="icon-container">
-              <span>icon</span>
-              <span>overcast clouds</span>
+              <span className="icon">icon</span>
+              <span className="icon-des">overcast clouds</span>
             </div>
             <div className="description">
-              <div className="location">
+              <div className="location border flex">
                 <p>location</p>
                 <span>las vegas</span>
               </div>
-              <div className="temperature border">
+              <div className="temperature border flex">
                 <p>temperature</p>
-                <span>58.3</span>
+                <span>58.3 c</span>
               </div>
-              <div className="humidity border">
+              <div className="humidity border flex">
                 <p>humidity</p>
                 <span>55%</span>
               </div>
-              <div className="wind-speed border">
+              <div className="wind-speed border flex">
                 <p>wind speed</p>
                 <span>14.99 mph</span>
               </div>
-              <div className="sunrise">
+              <div className="sunrise flex">
                 <p>sunrise</p>
                 <span>6:53 am</span>
               </div>
-              <div className="sunset border">
+              <div className="sunset border flex">
                 <p>sunset</p>
                 <span>6:46 pm</span>
               </div>
